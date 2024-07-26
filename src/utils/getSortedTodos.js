96eb954a832b9bbd78aee1426e151bfd220f8529
Ankush Singh <96817217+ankush-singh-zet@users.noreply.github.com>
2024-07-26 14:14:50 +0530

    changed object keys cases (#1)

--- src/utils/getSortedTodos.js
+++ src/utils/getSortedTodos.js
@@ -1,5 +1,5 @@
 const getSortedTodos = (todos) => {
-  const priorityOrder = { high: 1, medium: 2, low: 3 };
+  const priorityOrder = { High: 1, Medium: 2, Low: 3 };
   return [...todos].sort(
     (a, b) => priorityOrder[a.priority] - priorityOrder[b.priority]
   );
